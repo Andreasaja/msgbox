cf904180dac45da7dfb4a70d786a1b8c0479bcd7
Andreasaja <under.block22@gmail.com>
2022-12-28 09:49:41 +0700

    upload sc dr folder msgbox kedua

--- texmsg/texmsg/Form1.vb
+++ texmsg/texmsg/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("ini di 4.7")
+        MsgBox("ini di 4.7, setelah dilakukan update sc lalu upload ke git")
     End Sub
 End Class
